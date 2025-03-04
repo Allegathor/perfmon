36931c35e29310200302b615a0b3a94a91245ed4
allegathor <allegathor@gmail.com>
2025-03-04 13:55:50 +0300

    rm wg

--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -1,24 +1,17 @@
 package storage
 
-import (
-	"sync"
-)
-
 type Storage interface {
 	SetGauge(name string, v float64)
 	UpdateCounter(name string, v int64)
 }
 
 type MetricsStorage struct {
-	WG      *sync.WaitGroup
 	Gauge   map[string]float64
 	Counter map[string]int64
 }
 
 func NewMetrics() *MetricsStorage {
-	wg := &sync.WaitGroup{}
 	return &MetricsStorage{
-		WG:      wg,
 		Gauge:   make(map[string]float64),
 		Counter: make(map[string]int64),
 	}
