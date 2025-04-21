1376fc7b75634ace4561d359c7d3c8f5f3888c8b
allegathor <allegathor@gmail.com>
2025-04-21 21:47:09 +0300

    add check sum for requests and sign for response via middlewares

--- internal/monserv/monserv.go
+++ internal/monserv/monserv.go
@@ -14,13 +14,15 @@ import (
 type MonServ struct {
 	*http.Server
 	db     handlers.MDB
+	key    string
 	Router *chi.Mux
 	Logger *zap.SugaredLogger
 }
 
-func NewInstance(ctx context.Context, addr string, db handlers.MDB, l *zap.SugaredLogger) *MonServ {
+func NewInstance(ctx context.Context, addr string, db handlers.MDB, key string, l *zap.SugaredLogger) *MonServ {
 	s := &MonServ{
 		db:     db,
+		key:    key,
 		Router: chi.NewRouter(),
 		Logger: l,
 	}
@@ -34,29 +36,49 @@ func NewInstance(ctx context.Context, addr string, db handlers.MDB, l *zap.Sugar
 
 func (s *MonServ) MountHandlers() {
 	api := handlers.NewAPI(s.db, s.Logger)
-	s.Router.Use(middlewares.CreateLogger(s.Logger), middlewares.CreateCompress(s.Logger))
+	mw := chi.Middlewares{middlewares.CreateLogger(s.Logger), middlewares.CreateUncompressReq(s.Logger)}
+	// update-related middlewares
+	umw := make(chi.Middlewares, len(mw))
+	copy(umw, mw)
 
-	s.Router.Get("/", api.CreateRootHandler(""))
-	s.Router.Route("/update", func(r chi.Router) {
-		r.Post("/", api.UpdateRootHandler)
-		r.Route("/{type}/{name}/{value}", func(r chi.Router) {
-			r.Post("/", api.UpdateHandler)
+	if s.key != "" {
+		umw = append(umw, middlewares.CreateSumChecker(s.key, s.Logger))
+		umw = append(umw, middlewares.CreateSigner(s.key, s.Logger))
+	}
+	mw = append(mw, middlewares.CreateCompress(s.Logger))
+	umw = append(umw, middlewares.CreateCompress(s.Logger))
+
+	// main group
+	s.Router.Group(func(r chi.Router) {
+		r.Use(mw...)
+		r.Get("/", api.CreateRootHandler(""))
+
+		r.Route("/value", func(r chi.Router) {
+			r.Post("/", api.ValueRootHandler)
+			r.Route("/{type}/{name}", func(r chi.Router) {
+				r.Get("/", api.ValueHandler)
+			})
+		})
+
+		r.Route("/ping", func(r chi.Router) {
+			r.Get("/", api.PingHandler)
 		})
 	})
 
-	s.Router.Route("/updates", func(r chi.Router) {
-		r.Post("/", api.UpdateBatchHandler)
-	})
+	// update group
+	s.Router.Group(func(r chi.Router) {
+		r.Use(umw...)
 
-	s.Router.Route("/value", func(r chi.Router) {
-		r.Post("/", api.ValueRootHandler)
-		r.Route("/{type}/{name}", func(r chi.Router) {
-			r.Get("/", api.ValueHandler)
+		r.Route("/update", func(r chi.Router) {
+			r.Post("/", api.UpdateRootHandler)
+			r.Route("/{type}/{name}/{value}", func(r chi.Router) {
+				r.Post("/", api.UpdateHandler)
+			})
 		})
-	})
 
-	s.Router.Route("/ping", func(r chi.Router) {
-		r.Get("/", api.PingHandler)
+		r.Route("/updates", func(r chi.Router) {
+			r.Post("/", api.UpdateBatchHandler)
+		})
 	})
 
 	s.Handler = s.Router
